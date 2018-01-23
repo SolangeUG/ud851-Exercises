64f01b1b19a6bfa5258506e1c7944fdac916f458
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-23 15:24:13 +0100

    Toy app: complete the creating the contract exercise

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -4,14 +4,32 @@ import android.provider.BaseColumns;
 
 public class WaitlistContract {
 
+    /**
+     * Private constructor to prevent someone from
+     * accidentally instantiating the contract class
+     */
+    private WaitlistContract() {}
 
+    // COMPLETED (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
 
-    // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
+    /**
+     * Inner class that defines the table contents
+     */
+    static final class WaitlistEntry implements BaseColumns {
 
-        // TODO (2) Inside create a static final members for the table name and each of the db columns
+        // COMPLETED (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
         // COLUMN_TIMESTAMP -> timestamp
+
+        static final String TABLE_NAME = "waitlist";
+        static final String COLUMN_GUEST_NAME = "guestName";
+        static final String COLUMN_PARTY_SIZE = "partySize";
+        static final String COLUMN_TIMESTAMP = "timestamp";
+    }
+
+
+
 
 }
