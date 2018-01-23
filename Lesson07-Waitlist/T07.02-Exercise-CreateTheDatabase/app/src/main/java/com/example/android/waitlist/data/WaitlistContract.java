5a4b118a0e292a0ea78ce9167814b68bdd596972
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-23 15:53:51 +0100

    Toy app: complete the creating the database exercise

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -8,7 +8,7 @@ public class WaitlistContract {
         public static final String TABLE_NAME = "waitlist";
         public static final String COLUMN_GUEST_NAME = "guestName";
         public static final String COLUMN_PARTY_SIZE = "partySize";
-        public static final String COLUMN_TIMESTAMP = "timestamp";
+        static final String COLUMN_TIMESTAMP = "timestamp";
     }
 
 }
