4f127f4f2dde442ece888b47a3ce9ab9a87dab0f
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-25 15:02:25 +0100

    Toy app: complete the adding notification actions exercise

--- Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -30,6 +30,8 @@ import android.support.v4.content.ContextCompat;
 
 import com.example.android.background.MainActivity;
 import com.example.android.background.R;
+import com.example.android.background.sync.ReminderTasks;
+import com.example.android.background.sync.WaterReminderIntentService;
 
 /**
  * Utility class for creating hydration notifications
@@ -46,12 +48,19 @@ public class NotificationUtils {
      * This pending intent id is used to uniquely reference the pending intent
      */
     private static final int WATER_REMINDER_PENDING_INTENT_ID = 3417;
+    private static final int ACTION_IGNORE_PENDING_INTENT_ID = 5555;
+    private static final int ACTION_INCREMENT_WATER_ID = 4444;
     /**
      * This notification channel id is used to link notifications to this channel
      */
     private static final String WATER_REMINDER_NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
 
-    //  TODO (1) Create a method to clear all notifications
+    // COMPLETED (1) Create a method to clear all notifications
+    public static void clearAllNotifications(Context context) {
+        NotificationManager manager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancelAll();
+    }
 
     public static void remindUserBecauseCharging(Context context) {
         NotificationManager notificationManager = (NotificationManager)
@@ -73,7 +82,11 @@ public class NotificationUtils {
                         context.getString(R.string.charging_reminder_notification_body)))
                 .setDefaults(Notification.DEFAULT_VIBRATE)
                 .setContentIntent(contentIntent(context))
-                // TODO (17) Add the two new actions using the addAction method and your helper methods
+
+                // COMPLETED (17) Add the two new actions using the addAction method and your helper methods
+                .addAction(ignoreReminderAction(context))
+                .addAction(drinkWaterAction(context))
+
                 .setAutoCancel(true);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
@@ -83,20 +96,53 @@ public class NotificationUtils {
         notificationManager.notify(WATER_REMINDER_NOTIFICATION_ID, notificationBuilder.build());
     }
 
-    //  TODO (5) Add a static method called ignoreReminderAction
-    //      TODO (6) Create an Intent to launch WaterReminderIntentService
-    //      TODO (7) Set the action of the intent to designate you want to dismiss the notification
-    //      TODO (8) Create a PendingIntent from the intent to launch WaterReminderIntentService
-    //      TODO (9) Create an Action for the user to ignore the notification (and dismiss it)
-    //      TODO (10) Return the action
+    // COMPLETED (5) Add a static method called ignoreReminderAction
+    public static NotificationCompat.Action ignoreReminderAction(Context context) {
+        // COMPLETED (6) Create an Intent to launch WaterReminderIntentService
+        Intent intent = new Intent(context, WaterReminderIntentService.class);
 
+        // COMPLETED (7) Set the action of the intent to designate you want to dismiss the notification
+        intent.setAction(ReminderTasks.ACTION_DISMISS_NOTIFICATION);
 
-    //  TODO (11) Add a static method called drinkWaterAction
-    //      TODO (12) Create an Intent to launch WaterReminderIntentService
-    //      TODO (13) Set the action of the intent to designate you want to increment the water count
-    //      TODO (14) Create a PendingIntent from the intent to launch WaterReminderIntentService
-    //      TODO (15) Create an Action for the user to tell us they've had a glass of water
-    //      TODO (16) Return the action
+        // COMPLETED (8) Create a PendingIntent from the intent to launch WaterReminderIntentService
+        PendingIntent pendingIntent = PendingIntent.getService(context,
+                ACTION_IGNORE_PENDING_INTENT_ID,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // COMPLETED (9) Create an Action for the user to ignore the notification (and dismiss it)
+        NotificationCompat.Action ignoreAction = new NotificationCompat.Action(
+                R.drawable.ic_cancel_black_24px,
+                "No, thanks.",
+                pendingIntent);
+
+        // COMPLETED (10) Return the action
+        return ignoreAction;
+    }
+
+    // COMPLETED (11) Add a static method called drinkWaterAction
+    public static NotificationCompat.Action drinkWaterAction(Context context) {
+        // COMPLETED (12) Create an Intent to launch WaterReminderIntentService
+        Intent intent = new Intent(context, WaterReminderIntentService.class);
+
+        // COMPLETED (13) Set the action of the intent to designate you want to increment the water count
+        intent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+
+        // COMPLETED (14) Create a PendingIntent from the intent to launch WaterReminderIntentService
+        PendingIntent pendingIntent = PendingIntent.getService(context,
+                ACTION_INCREMENT_WATER_ID,
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // COMPLETED (15) Create an Action for the user to tell us they've had a glass of water
+        NotificationCompat.Action drinkAction = new NotificationCompat.Action(
+                R.drawable.ic_local_drink_black_24px,
+                "I did it!",
+                pendingIntent);
+
+        // COMPLETED (16) Return the action
+        return drinkAction;
+    }
 
 
     private static PendingIntent contentIntent(Context context) {
